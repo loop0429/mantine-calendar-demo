573a5cb6fdf3d4ae664b980321008e41edc9dff8
loop0429 <model.f.n@gmail.com>
2023-01-11 22:14:03 +0900

    土曜日が青文字で表示されるように

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import { Stack } from "@mantine/core";
 import { Calendar } from "@mantine/dates";
 import { ThemeProvider } from "./ThemeProvider";
 import "dayjs/locale/ja";
 
 export default function App() {
+  const [value, setValue] = useState<Date | null>(null);
+
   return (
     <ThemeProvider>
       <Stack align="center" mt={50}>
         <Calendar
+          dayStyle={(date, modifiers) => {
+            // 土曜日以外、outiside（表示月以外の日付）であれば早期リターン
+            // 戻り値の型がReact.CSSPropertiesなので{}で返す
+            if (date.getDay() !== 6 || modifiers.outside === true) {
+              return {}
+            }
+
+            // 単純に青(blue)で返すと選択時も青になるため、選択時は白で返す
+            const color = modifiers.selected ? 'white' : 'blue'
+            return { color }
+          }}
           firstDayOfWeek="sunday"
           labelFormat="YYYY年MM月"
           locale="ja"
+          value={value}
+          onChange={setValue}
         />
       </Stack>
     </ThemeProvider>
