a162f324ecd80393bef55427603dd090583d712a
loop0429 <model.f.n@gmail.com>
2023-04-25 22:36:41 +0900

    v6対応版

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Stack } from "@mantine/core";
-import { Calendar } from "@mantine/dates";
+import { DatePicker } from "@mantine/dates";
 import { ThemeProvider } from "./ThemeProvider";
 import "dayjs/locale/ja";
 
@@ -10,20 +10,21 @@ export default function App() {
   return (
     <ThemeProvider>
       <Stack align="center" mt={50}>
-        <Calendar
-          dayStyle={(date, modifiers) => {
-            // 土曜日以外、outside（表示月以外の日付）であれば早期リターン
-            // 戻り値の型がReact.CSSPropertiesなので{}で返す
-            if (date.getDay() !== 6 || modifiers.outside === true) {
-              return {}
+        <DatePicker
+          getDayProps={(date) => {
+            if (date.getDay() === 6) {
+              return {
+                sx: (theme) => ({
+                  color: theme.colors.blue,
+                })
+              }
             }
 
-            // 単純に青(blue)で返すと選択時も青になるため、選択時は白で返す
-            const color = modifiers.selected ? 'white' : 'blue'
-            return { color }
+            return {}
           }}
-          firstDayOfWeek="sunday"
-          labelFormat="YYYY年MM月"
+          firstDayOfWeek={0}
+          weekendDays={[0]}
+          monthLabelFormat="YYYY年MMM"
           locale="ja"
           value={value}
           onChange={setValue}
